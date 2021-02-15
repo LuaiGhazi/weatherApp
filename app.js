6bc7313127bd0763d70ae8c70d6a741d9d3779c6
LuaiGhazi <luaighazi@gmail.com>
2021-02-15 10:21:02 -0800

    Refactored Code

--- app.js
+++ app.js
@@ -1,71 +1,79 @@
+const forecastCardsElement = document.querySelector("#forecastCards")
 const form = document.querySelector("#searchForm");
+
 //CurrentDay
 const currentWeather = document.querySelector('#currentWeather')
 const currentWeatherDesc = document.querySelector('#currentWeatherDesc')
 const currentDayImg = document.querySelector('#currentDayImg')
-const feelsLike = document.querySelector('#feelsLike')
-const humidity = document.querySelector('#humidity')
-const pressure = document.querySelector('#pressure')
-const averageTemp = document.querySelector('#averageTemp')
-const tempMax = document.querySelector('#tempMax')
-const tempMin = document.querySelector('#tempMin')
-//DayOneForecast
-const forecastOneWeather = document.querySelector('#forecastOneWeather')
-const forecastOneWeatherDesc = document.querySelector('#forecastOneWeatherDesc')
-const forecastOneImg = document.querySelector('#forecastOneImg')
-const forecastOneFeelsLike = document.querySelector('#forecastOneFeelsLike')
-const forecastOneHumidity = document.querySelector('#forecastOneHumidity')
-const forecastOnePressure = document.querySelector('#forecastOnePressure')
-const forecastOneAverageTemp = document.querySelector('#forecastOneAverageTemp')
-const forecastOneTempMax = document.querySelector('#forecastOneTempMax')
-const forecastOneTempMin = document.querySelector('#forecastOneTempMin')
-//DayTwoForecast
-const forecastTwoWeather = document.querySelector('#forecastTwoWeather')
-const forecastTwoWeatherDesc = document.querySelector('#forecastTwoWeatherDesc')
-const forecastTwoImg = document.querySelector('#forecastTwoImg')
-const forecastTwoFeelsLike = document.querySelector('#forecastTwoFeelsLike')
-const forecastTwoHumidity = document.querySelector('#forecastTwoHumidity')
-const forecastTwoPressure = document.querySelector('#forecastTwoPressure')
-const forecastTwoAverageTemp = document.querySelector('#forecastTwoAverageTemp')
-const forecastTwoTempMax = document.querySelector('#forecastTwoTempMax')
-const forecastTwoTempMin = document.querySelector('#forecastTwoTempMin')
-//DayThreeForecast
-const forecastThreeWeather = document.querySelector('#forecastThreeWeather')
-const forecastThreeWeatherDesc = document.querySelector('#forecastThreeWeatherDesc')
-const forecastThreeImg = document.querySelector('#forecastThreeImg')
-const forecastThreeFeelsLike = document.querySelector('#forecastThreeFeelsLike')
-const forecastThreeHumidity = document.querySelector('#forecastThreeHumidity')
-const forecastThreePressure = document.querySelector('#forecastThreePressure')
-const forecastThreeAverageTemp = document.querySelector('#forecastThreeAverageTemp')
-const forecastThreeTempMax = document.querySelector('#forecastThreeTempMax')
-const forecastThreeTempMin = document.querySelector('#forecastThreeTempMin')
-//DayFourForecast
-const forecastFourWeather = document.querySelector('#forecastFourWeather')
-const forecastFourWeatherDesc = document.querySelector('#forecastFourWeatherDesc')
-const forecastFourImg = document.querySelector('#forecastFourImg')
-const forecastFourFeelsLike = document.querySelector('#forecastFourFeelsLike')
-const forecastFourHumidity = document.querySelector('#forecastFourHumidity')
-const forecastFourPressure = document.querySelector('#forecastFourPressure')
-const forecastFourAverageTemp = document.querySelector('#forecastFourAverageTemp')
-const forecastFourTempMax = document.querySelector('#forecastFourTempMax')
-const forecastFourTempMin = document.querySelector('#forecastFourTempMin')
-//DayFiveForecast
-const forecastFiveWeather = document.querySelector('#forecastFiveWeather')
-const forecastFiveWeatherDesc = document.querySelector('#forecastFiveWeatherDesc')
-const forecastFiveImg = document.querySelector('#forecastFiveImg')
-const forecastFiveFeelsLike = document.querySelector('#forecastFiveFeelsLike')
-const forecastFiveHumidity = document.querySelector('#forecastFiveHumidity')
-const forecastFivePressure = document.querySelector('#forecastFivePressure')
-const forecastFiveAverageTemp = document.querySelector('#forecastFiveAverageTemp')
-const forecastFiveTempMax = document.querySelector('#forecastFiveTempMax')
-const forecastFiveTempMin = document.querySelector('#forecastFiveTempMin')
-// Day of week 
+const currentFeelsLike = document.querySelector('#feelsLike')
+const currentHumidity = document.querySelector('#humidity')
+const currentPressure = document.querySelector('#pressure')
+const currentAverageTemp = document.querySelector('#averageTemp')
+const currentTempMax = document.querySelector('#tempMax')
+const currentTempMin = document.querySelector('#tempMin')
 const dayZero = document.querySelector('#dayZero')
-const dayOne = document.querySelector('#dayOne')
-const dayTwo = document.querySelector('#dayTwo')
-const dayThree = document.querySelector('#dayThree')
-const dayFour = document.querySelector('#dayFour')
-const dayFive = document.querySelector('#dayFive')
+
+
+
+let forecast_days = ['forecastOne', 'forecastTwo', 'forecastThree', 'forecastFour', 'forecastFive']
+
+
+
+function createWeatherCards() {
+    forecast_days.forEach((forecastDay) => {
+        //Create the column div 
+        forecastCardsElement.innerHTML += `<div class="col"></div>`;
+        const col = document.querySelector(".col:last-child");
+        col.innerHTML += `<div class="card h-100">
+        <div class="card-header">
+            <span id='${forecastDay}Day'> </span>
+        </div>
+        <img src="imgs/clear.jpg" class="card-img-top" id='${forecastDay}Img' alt="...">
+        <div class="card-body">
+            <h5 class="card-title" id='${forecastDay}Weather'> </h5>
+            <h5 class="card-title"><span id='${forecastDay}AverageTemp'></span><span>&#8451;</span></h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                    <p><b>Description</b></p>
+                    <span id='${forecastDay}WeatherDesc'> </span>
+                </li>
+                <li class="list-group-item">
+                    <p><b>Feels Like</b></p>
+                    <span id='${forecastDay}FeelsLike'> </span>
+                    <span>&#8451;</span>
+                </li>
+                <li class="list-group-item">
+                    <p><b>Temp Max</b></p>
+                    <span id='${forecastDay}TempMax'> </span>
+                    <span>&#8451;</span>
+                </li>
+
+                <li class="list-group-item">
+                    <p><b>Temp Min </b></p>
+                    <span id='${forecastDay}TempMin'> </span>
+                    <span>&#8451;</span>
+                </li>
+
+                <li class="list-group-item">
+                    <p><b>Humidity</b></p>
+                    <span id='${forecastDay}Humidity'> </span>
+                    <span>%</span>
+                </li>
+
+                <li class="list-group-item">
+                    <p><b>Pressure</b></p> <span id='${forecastDay}Pressure'> </span>
+                    <span>Pa</span>
+                </li>
+            </ul>
+        </div>
+        <div class="card-footer">
+            <small class="text-muted">Last updated 3 mins ago</small>
+        </div>
+    </div>`
+    });
+}
+
+createWeatherCards()
 
 //TitleCase
 function toTitleCase(str) {
@@ -96,18 +104,20 @@ form.addEventListener('submit', async function (e) {
     //Using axios for the get request
     const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&APPID=18893db06dc07b8725a3235643189f47&units=metric`)
     const res_forecast = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?q=${searchTerm}&appid=18893db06dc07b8725a3235643189f47&units=metric`)
-    console.log(res_forecast.data)
-    // console.log(res.data.main.feels_like)
+
     //Today's Forecast 
     currentWeather.innerHTML = res.data.weather[0].main
     currentWeatherDesc.innerHTML = toTitleCase(res.data.weather[0].description)
+    // console.log(res.data)
+    // console.log(res_forecast.data)
+    // console.log(res_forecast.data.list[0].dt_txt)
     dayZero.innerHTML = dayofWeek(res_forecast.data.list[0].dt_txt.substring(0, 10))
-    console.log(dayofWeek(res_forecast.data.list[0].dt_txt.substring(0, 10)))
-
     if (currentWeather.innerHTML === 'Clouds') {
         currentDayImg.src = 'imgs/cloudy.jpg'
     } else if (currentWeather.innerHTML === 'Clear') {
         currentDayImg.src = 'imgs/clear.jpg'
+    } else if (currentWeather.innerHTML === 'Rain') {
+        currentDayImg.src = 'imgs/rain.png'
     } else {
         currentDayImg.src = 'imgs/snow.jpg'
     }
@@ -119,100 +129,36 @@ form.addEventListener('submit', async function (e) {
     tempMin.innerHTML = Math.round(res.data.main.temp_min)
 
     //Tomorrow's Forecast 
-    forecastOneWeather.innerHTML = res_forecast.data.list[3].weather[0].main
-    forecastOneWeatherDesc.innerHTML = toTitleCase(res_forecast.data.list[3].weather[0].description)
-    dayOne.innerHTML = dayofWeek(res_forecast.data.list[3].dt_txt.substring(0, 10))
-    if (forecastOneWeather.innerHTML === 'Clouds') {
-        forecastOneImg.src = 'imgs/cloudy.jpg'
-    } else if (forecastOneWeather.innerHTML === 'Clear') {
-        forecastOneImg.src = 'imgs/clear.jpg'
-    } else {
-        forecastOneImg.src = 'imgs/snow.jpg'
-    }
-    forecastOneFeelsLike.innerHTML = Math.round(res_forecast.data.list[3].main.feels_like)
-    forecastOneHumidity.innerHTML = Math.round(res_forecast.data.list[3].main.humidity)
-    forecastOnePressure.innerHTML = res_forecast.data.list[3].main.pressure
-    forecastOneAverageTemp.innerHTML = Math.round(res_forecast.data.list[3].main.temp)
-    forecastOneTempMax.innerHTML = Math.round(res_forecast.data.list[3].main.temp_max)
-    forecastOneTempMin.innerHTML = Math.round(res_forecast.data.list[3].main.temp_min)
-
-    //Day Two Forecast 
-    forecastTwoWeather.innerHTML = res_forecast.data.list[11].weather[0].main
-    forecastTwoWeatherDesc.innerHTML = toTitleCase(res_forecast.data.list[11].weather[0].description)
-    dayTwo.innerHTML = dayofWeek(res_forecast.data.list[11].dt_txt.substring(0, 10))
-    if (forecastTwoWeather.innerHTML === 'Clouds') {
-        forecastTwoImg.src = 'imgs/cloudy.jpg'
-    } else if (forecastTwoWeather.innerHTML === 'Clear') {
-        forecastTwoImg.src = 'imgs/clear.jpg'
-    } else {
-        forecastTwoImg.src = 'imgs/snow.jpg'
-    }
-    forecastTwoFeelsLike.innerHTML = Math.round(res_forecast.data.list[11].main.feels_like)
-    forecastTwoHumidity.innerHTML = Math.round(res_forecast.data.list[11].main.humidity)
-    forecastTwoPressure.innerHTML = res_forecast.data.list[11].main.pressure
-    forecastTwoAverageTemp.innerHTML = Math.round(res_forecast.data.list[11].main.temp)
-    forecastTwoTempMax.innerHTML = Math.round(res_forecast.data.list[11].main.temp_max)
-    forecastTwoTempMin.innerHTML = Math.round(res_forecast.data.list[11].main.temp_min)
-
-    // Day Three
-    forecastThreeWeather.innerHTML = res_forecast.data.list[19].weather[0].main
-    forecastThreeWeatherDesc.innerHTML = toTitleCase(res_forecast.data.list[19].weather[0].description)
-    dayThree.innerHTML = dayofWeek(res_forecast.data.list[19].dt_txt.substring(0, 10))
-    if (forecastThreeWeather.innerHTML === 'Clouds') {
-        forecastThreeImg.src = 'imgs/cloudy.jpg'
-    } else if (forecastThreeWeather.innerHTML === 'Clear') {
-        forecastThreeImg.src = 'imgs/clear.jpg'
-    } else {
-        forecastThreeImg.src = 'imgs/snow.jpg'
-    }
-    forecastThreeFeelsLike.innerHTML = Math.round(res_forecast.data.list[19].main.feels_like)
-    forecastThreeHumidity.innerHTML = Math.round(res_forecast.data.list[19].main.humidity)
-    forecastThreePressure.innerHTML = res_forecast.data.list[19].main.pressure
-    forecastThreeAverageTemp.innerHTML = Math.round(res_forecast.data.list[19].main.temp)
-    forecastThreeTempMax.innerHTML = Math.round(res_forecast.data.list[19].main.temp_max)
-    forecastThreeTempMin.innerHTML = Math.round(res_forecast.data.list[19].main.temp_min)
-
-    //Day4
-    forecastFourWeather.innerHTML = res_forecast.data.list[27].weather[0].main
-    forecastFourWeatherDesc.innerHTML = toTitleCase(res_forecast.data.list[27].weather[0].description)
-    dayFour.innerHTML = dayofWeek(res_forecast.data.list[27].dt_txt.substring(0, 10))
-    if (forecastFourWeather.innerHTML === 'Clouds') {
-        forecastFourImg.src = 'imgs/cloudy.jpg'
-    } else if (forecastFourWeather.innerHTML === 'Clear') {
-        forecastFourImg.src = 'imgs/clear.jpg'
-    } else {
-        forecastFourImg.src = 'imgs/snow.jpg'
-    }
-    forecastFourFeelsLike.innerHTML = Math.round(res_forecast.data.list[27].main.feels_like)
-    forecastFourHumidity.innerHTML = Math.round(res_forecast.data.list[27].main.humidity)
-    forecastFourPressure.innerHTML = res_forecast.data.list[27].main.pressure
-    forecastFourAverageTemp.innerHTML = Math.round(res_forecast.data.list[27].main.temp)
-    forecastFourTempMax.innerHTML = Math.round(res_forecast.data.list[27].main.temp_max)
-    forecastFourTempMin.innerHTML = Math.round(res_forecast.data.list[27].main.temp_min)
-
-    //Day5
-    forecastFiveWeather.innerHTML = res_forecast.data.list[35].weather[0].main
-    forecastFiveWeatherDesc.innerHTML = toTitleCase(res_forecast.data.list[35].weather[0].description)
-    dayFive.innerHTML = dayofWeek(res_forecast.data.list[35].dt_txt.substring(0, 10))
-    if (forecastFiveWeather.innerHTML === 'Clouds') {
-        forecastFiveImg.src = 'imgs/cloudy.jpg'
-    } else if (forecastFiveWeather.innerHTML === 'Clear') {
-        forecastFiveImg.src = 'imgs/clear.jpg'
-    } else {
-        forecastFiveImg.src = 'imgs/snow.jpg'
-    }
-    forecastFiveFeelsLike.innerHTML = Math.round(res_forecast.data.list[35].main.feels_like)
-    forecastFiveHumidity.innerHTML = Math.round(res_forecast.data.list[35].main.humidity)
-    forecastFivePressure.innerHTML = res_forecast.data.list[35].main.pressure
-    forecastFiveAverageTemp.innerHTML = Math.round(res_forecast.data.list[35].main.temp)
-    forecastFiveTempMax.innerHTML = Math.round(res_forecast.data.list[35].main.temp_max)
-    forecastFiveTempMin.innerHTML = Math.round(res_forecast.data.list[35].main.temp_min)
+    let i = 3
+    forecast_days.forEach((forecastDay) => {
+        const weather = document.querySelector(`#${forecastDay}Weather`)
+        const weatherDesc = document.querySelector(`#${forecastDay}WeatherDesc`)
+        const day = document.querySelector(`#${forecastDay}Day`)
+        const img = document.querySelector(`#${forecastDay}Img`)
+        const feelsLike = document.querySelector(`#${forecastDay}FeelsLike`)
+        const humidity = document.querySelector(`#${forecastDay}Humidity`)
+        const pressure = document.querySelector(`#${forecastDay}Pressure`)
+        const averageTemp = document.querySelector(`#${forecastDay}AverageTemp`)
+        const tempMax = document.querySelector(`#${forecastDay}TempMax`)
+        const tempMin = document.querySelector(`#${forecastDay}TempMin`)
+        weather.innerHTML = res_forecast.data.list[i].weather[0].main
+        weatherDesc.innerHTML = toTitleCase(res_forecast.data.list[i].weather[0].description)
+        day.innerHTML = dayofWeek(res_forecast.data.list[i].dt_txt.substring(0, 10))
+        if (weather.innerHTML === 'Clouds') {
+            img.src = 'imgs/cloudy.jpg'
+        } else if (weather.innerHTML === 'Clear') {
+            img.src = 'imgs/clear.jpg'
+        } else if (weather.innerHTML === 'Rain') {
+            img.src = 'imgs/rain.png'
+        } else {
+            img.src = 'imgs/snow.jpg'
+        }
+        feelsLike.innerHTML = Math.round(res_forecast.data.list[i].main.feels_like)
+        humidity.innerHTML = Math.round(res_forecast.data.list[i].main.humidity)
+        pressure.innerHTML = res_forecast.data.list[i].main.pressure
+        averageTemp.innerHTML = Math.round(res_forecast.data.list[i].main.temp)
+        tempMax.innerHTML = Math.round(res_forecast.data.list[i].main.temp_max)
+        tempMin.innerHTML = Math.round(res_forecast.data.list[i].main.temp_min)
+        i += 8
+    })
 })
-
-
-
-
-// let daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-// let now = new Date()
-
-// dayZero.innerHTML = daysOfWeek[now.getDay()]
